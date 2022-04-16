a197fc36629764168832334deadee55d81f8a89d
DM0R3N0 <dfm84@hotmail.com>
2022-04-16 13:14:36 -0300

    Initial commit

---
+++ src/main/java/br/com/daniel/athenas/DanielCadastroAthenasApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniel.athenas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielCadastroAthenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielCadastroAthenasApplication.class, args);
+	}
+
+}
